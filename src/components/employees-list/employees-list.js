7ba4f70b7089f10ce9f5dadb6d0b1b5d35779302
Murvat <murvat.mustafayev@mail.ru>
2024-07-05 09:55:54 +0100

    passingProps

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -6,10 +6,15 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 import './employees-list.css';
 
 const EmployeesList = ({ data }) => {
-    let element = data.map(item => (<EmployeesListItem {...item} />))
+    const elements = data.map(item => {
+        const { id, ...itemProps } = item;
+        return (
+            <EmployeesListItem key={id} {...itemProps} />
+        );
+    })
     return (
         <ul className="app-list list-group">
-            {element}
+            {elements}
         </ul>
     )
 }
